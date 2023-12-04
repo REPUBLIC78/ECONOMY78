c858964571d3bb5d3cf8f2894fbf14dc8e14fdee
neonel <Limard62@gmail.com>
2023-12-05 02:00:05 +0300

    Сокращение кода Tk.py

--- Tk.py
+++ Tk.py
@@ -17,33 +17,14 @@ vd = {
     'QCR': None
 }
 
-vl, cl = list(vd), []
+vl, cl, rl = list(vd), [], []#список ключей, пустой список для наименований полей, список с полями ввода
 
 def Cliсked():
-    if TCtxt.get() != '':vd['TC'] = int(TCtxt.get())
-    else:vd['TC'] = None
-    if VCtxt.get() != '':vd['VC'] = int(VCtxt.get())
-    else:vd['VC'] = None
-    if FCtxt.get() != '':vd['FC'] = int(FCtxt.get())
-    else:vd['FC'] = None
-    if AFCtxt.get() != '':vd['AFC'] = int(AFCtxt.get())
-    else:vd['AFC'] = None
-    if AVCtxt.get() != '':vd['AVC'] = int(AVCtxt.get())
-    else:vd['AVC'] = None
-    if Qtxt.get() != '':vd['Q'] = int(Qtxt.get())
-    else:vd['Q'] = None
-    if Ptxt.get() != '':vd['P'] = int(Ptxt.get())
-    else:vd['P'] = None
-    if ATCtxt.get() != '':vd['ATC'] = int(ATCtxt.get())
-    else:vd['ATC'] = None
-    if TRtxt.get() != '':vd['TR'] = int(TRtxt.get())
-    else:vd['TR'] = None
-    if PRtxt.get() != '':vd['PR'] = int(PRtxt.get())
-    else:vd['PR'] = None
-    if ARtxt.get() != '':vd['AR'] = int(PRtxt.get())
-    else:vd['AR'] = None
-    if QCRtxt.get() != '':vd['QCR'] = int(PRtxt.get())
-    else:vd['QCR'] = None
+    for i in range(12):
+        try:
+            vd[vl[i]] = int(rl[i].get())
+        except:
+            vd[vl[i]] = None
     if fTC(vd) == 'Недостаточно данных':
         ANSlbl.configure(text='Н/д')
     else:
@@ -55,53 +36,21 @@ window.geometry('640x360')
 window.minsize(500,300)
 window.maxsize(640,360)
 
-for c in range(12): window.columnconfigure(index=c, weight=1)
+for c in range(len(vd)): window.columnconfigure(index=c, weight=1) #Адаптив
 for r in range(4): window.rowconfigure(index=r, weight=1)
 
-for i in range(12):
-    cl.append(Label(window, text=vl[i], font=('Arial Bold', 14)))
+for i in range(len(vd)):
+    cl.append(Label(window, text=vl[i], font=('Arial Bold', 14))) #названия полей ввода
     cl[i].grid(column=i, row=0)
 
-TCtxt = Entry(window, width=6)
-TCtxt.grid(column=0, row=1)
+for i in range(12):
+    rl.append(Entry(window, width=6)) #поля ввода
+    rl[i].grid(column=i, row=1)
 
-VCtxt = Entry(window, width=6)
-VCtxt.grid(column=1, row=1)
-
-FCtxt = Entry(window, width=6)
-FCtxt.grid(column=2, row=1)
-
-AFCtxt = Entry(window, width=6)
-AFCtxt.grid(column=3, row=1)
-
-AVCtxt = Entry(window, width=6)
-AVCtxt.grid(column=4, row=1)
-
-Qtxt = Entry(window, width=6)
-Qtxt.grid(column=5, row=1)
-
-Ptxt = Entry(window, width=6)
-Ptxt.grid(column=6, row=1)
-
-ATCtxt = Entry(window, width=6)
-ATCtxt.grid(column=7, row=1)
-
-TRtxt = Entry(window, width=6)
-TRtxt.grid(column=8, row=1)
-
-PRtxt = Entry(window, width=6)
-PRtxt.grid(column=9, row=1)
-
-ARtxt = Entry(window, width=6)
-ARtxt.grid(column=10, row=1)
-
-QCRtxt = Entry(window, width=6)
-QCRtxt.grid(column=11, row=1)
-
-btn = Button(window, text='Рассчитать', command=Cliсked, width=8)
+btn = Button(window, text='Рассчитать', command=Cliсked, width=8) #кнопка рассчитать
 btn.grid(column=5, row=2, columnspan=2)
 
-ANSlbl = Label(window, text='TC = ', font=('Arial Bold', 14))
+ANSlbl = Label(window, text='TC = ', font=('Arial Bold', 14)) #вывод ответа
 ANSlbl.grid(column=5, row=3, columnspan=2)
 
 window.mainloop()
